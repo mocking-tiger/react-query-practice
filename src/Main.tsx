602b298533dfb53f3c29f7f0d0ee19f056672051
Park JunYong <dyden119@gmail.com>
2024-11-19 20:55:14 +0900

    chore: react router dom설치, 라우팅, 메인 컴포넌트 생성

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,16 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./components/App";
 import HomePage from "./pages/HomePage";
 
 const queryClient = new QueryClient();
 
-function App() {
+export default function Main() {
   return (
     <QueryClientProvider client={queryClient}>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
-
-export default App;
